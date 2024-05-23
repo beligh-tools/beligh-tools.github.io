f1b6a5388c711ded6c5483a2c3b91a38d878c7a6
Beligh HAMDI <beligh@Belighs-MacBook-Pro.local>
2024-05-22 22:38:51 -0400

    fix pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,9 +9,11 @@ import { provideServiceWorker } from '@angular/service-worker';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withHashLocation()),
-    provideHttpClient(), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
-          })
+    })
   ]
 };
